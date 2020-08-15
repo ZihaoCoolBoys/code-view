b30dc5f95661bc13130663b89e30b6d6cf7bc875
子豪 <416605153@qq.com>
2020-08-15 18:32:27 +0800

    统计信息输出的另外一种形式,将数据进行网页形式展示

---
+++ src/main/java/com/zihao/out/HtmlOut.java
@@ -0,0 +1,15 @@
+package com.zihao.out;
+
+/**
+ * 将统计信息进行网页形式输出
+ * date: 2020-8-15
+ * by: zi hao
+ */
+
+public class HtmlOut extends OutCountResult {
+
+    @Override
+    void print() throws InterruptedException {
+
+    }
+}
